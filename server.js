0386cd68c30e6441944a71b50b3f8b24a560b6cd
kelly <kelly@example.com>
2025-12-16 01:48:48 +0000

    Fix ContactMessage usage in contact route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const db = require('./models');
 const { ContactMessage } = db;
-require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
